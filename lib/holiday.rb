df3ee959e84d73cee22be876ec72021001d9679b
Melissa Hummelt <mkhummelt@gmail.com>
2019-02-06 02:36:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,5 +73,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_has.each do |season, holiday|
+    holiday.each do |holiday, supplies|
+      if supplies.include? "BBQ"
 end
