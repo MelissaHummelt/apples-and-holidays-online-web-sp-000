72e07914e8370e8f5509c682973be0c2da074841
Melissa Hummelt <mkhummelt@gmail.com>
2019-02-06 02:06:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
-      puts "#{holiday.to_s}: #{supplies}"
+      puts "#{holiday.to_s.split("_")}: #{supplies}"
+      #{}"  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
     end
   end
 end
