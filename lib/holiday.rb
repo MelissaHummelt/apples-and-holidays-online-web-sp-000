6c0dfc4cc74ad4f2a03a387cfa6721bee921acc2
Melissa Hummelt <mkhummelt@gmail.com>
2019-02-06 01:52:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, supplies|
+      puts "#{holiday}: #{supplies}"
+    end
+    end
 end
 
 def all_holidays_with_bbq(holiday_hash)
